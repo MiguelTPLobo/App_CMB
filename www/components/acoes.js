127bfa0ad8cef54fd1913be569bb0b6c0c773f53
MiguelTPLobo <migueltrigoplobo@gmail.com>
2019-03-28 03:24:15 +0900

    ACABOU O K O

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -16,6 +16,13 @@ function cancelta(){
   document.getElementById('IM').hidden = !false;
 }
 
+function Etapa3(){
+  document.getElementById('SX').hidden = !false;
+  document.getElementById('TA').hidden = !false;
+  document.getElementById('IM').hidden = !false;
+  document.getElementById('Res').hidden = !true;
+}
+
 function Calcular(){
   var SX = getRadioValor('optradioSX');
   var TA = parseFloat(getRadioValor('optradioTA'));
@@ -23,7 +30,20 @@ function Calcular(){
   var A = parseInt(document.getElementById('altura').value);
   var I = parseInt(document.getElementById('idade').value);
 
+  if(SX == "Masculino"){
+    var MB = TA * (66 + ((13,7 *P + ( 5 * A) -(6,8 * I))));
+    document.getElementById('calorias').value = MB.toFixed(2);
+  }
+  else{
+    var MB = TA * (655 + ((9,6 *P + ( 1,8 * A) -(4,7 * I))));
+    document.getElementById('calorias').value = MB.toFixed(2)
+  }
+
   
+}
+
+function Reload(){
+  window.location.reload();
 }
 
 function getRadioValor(name){
